19049b1be38309167917f8d1926234b210e5a22d
codec <codec@fnord.cx>
2012-05-27 07:32:46 +0200

    fix template & test

--- tests/init.pp
+++ tests/init.pp
@@ -16,8 +16,24 @@ bind::zone { 'example.com':
   }
 }
 
+bind::zone { 'sub.example.com':
+  nameservers => ['192.168.0.1', '192.168.0.2'],
+  options     => {
+    'rr'  => 'master',
+  },
+  source      => 'puppet:///private/bind/db.sub.example.com'
+}
+
 bind::zone::record { 'test.example.com':
   zone  => 'example.com',
+  rr  => 'AAAA',
   key   => 'test',
-  value => '127.0.0.1'
+  value => '::1'
+}
+
+bind::zone::record { 'a_test.example.com':
+  zone  => 'example.com',
+  rr  => 'A',
+  key   => 'test',
+  value => '127.0.0.2'
 }
